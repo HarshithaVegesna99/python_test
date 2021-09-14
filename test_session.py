ea1e4bebc834af1fbe826a878b4cf2f4c6bb8d2b
HarshithaVegesna99 <76994452+HarshithaVegesna99@users.noreply.github.com>
2021-09-14 17:42:41 +0530

    Update test_session.py

--- test_session.py
+++ test_session.py
@@ -3,6 +3,7 @@ import pytest
 
 # import the function from your file
 from session import add_numbers
+from session import sub_numbers
 
 # Check if properly adds positive numbers
 def test_add_positive():
@@ -20,3 +21,21 @@ def test_add_negative():
 def test_add_string_expect_exception():
     with pytest.raises(TypeError):
         add_numbers(4, 'I do not belong here')
+        
+        
+# Check if properly adds positive numbers
+def test_sub_positive():
+    assert sub_numbers(2,1) == 1
+
+# Check if properly subtracts zero
+def test_sub_zero():
+    assert sub_numbers(1,0) == 1
+
+# Check with negative numbers
+def test_sub_negative():
+    assert add_numbers(100,-4) == 104
+
+# Now check if it correctly produces error when provided so
+def test_sub_string_expect_exception():
+    with pytest.raises(TypeError):
+        sub_numbers(4, 'I do not belong here')
